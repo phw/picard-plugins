874b13d223a95d34df502b46a1de0c3185027670
Philipp Wolfer <phil@parolu.io>
2019-07-15 19:07:20 +0200

    opencc: Added Chinese script conversion script functions
    Added $convert_to_simplified_chinese(text) and $convert_to_traditional_chinese(text)

--- plugins/opencc/__init__.py
+++ plugins/opencc/__init__.py
@@ -21,16 +21,16 @@ PLUGIN_NAME = 'Chinese script conversion'
 PLUGIN_AUTHOR = 'Philipp Wolfer'
 PLUGIN_DESCRIPTION = ('Convert track listings between Traditional Chinese and'
                       'Simplified Chinese script.')
-PLUGIN_VERSION = "1.0.0"
+PLUGIN_VERSION = "1.1.0"
 PLUGIN_API_VERSIONS = ["2.0", "2.1", "2.2", "2.3"]
 PLUGIN_LICENSE = "MIT"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 
-import picard.plugins.opencc.opencc
-
 from picard.album import Album
 from picard.cluster import Cluster
+from picard.plugins.opencc.opencc import OpenCC
+from picard.script import register_script_function
 from picard.track import Track
 from picard.ui.itemviews import (
     BaseAction,
@@ -44,15 +44,18 @@ from picard.ui.itemviews import (
 class ConvertChineseAction(BaseAction):
     def __init__(self, config):
         super().__init__()
-        self.converter = opencc.OpenCC(config=config)
+        self.converter = OpenCC(config=config)
 
     def callback(self, objs):
         for obj in objs:
             self.convert_object_metadata(obj)
 
+    def convert(self, text):
+        return self.converter.convert(text)
+
     def convert_metadata(self, m):
         for key, value in m.items():
-            m[key] = self.converter.convert(value)
+            m[key] = self.convert(value)
 
     def convert_object_metadata(self, obj):
         if hasattr(obj, 'metadata'):
@@ -70,29 +73,45 @@ class ConvertChineseAction(BaseAction):
                 self.convert_object_metadata(file)
 
 
-class ConvertToSimplifiedChinese(ConvertChineseAction):
+class SimplifiedChineseConverter(ConvertChineseAction):
     NAME = "Convert to Simplified Chinese"
 
     def __init__(self):
         super().__init__('t2s.json')
 
 
-class ConvertToTraditionalChinese(ConvertChineseAction):
+class TraditionalChineseConverter(ConvertChineseAction):
     NAME = "Convert to Traditional Chinese"
 
     def __init__(self):
         super().__init__('s2t.json')
 
 
-convert_to_simplified = ConvertToSimplifiedChinese()
-convert_to_traditional = ConvertToTraditionalChinese()
+simplified_converter = SimplifiedChineseConverter()
+traditional_converter = TraditionalChineseConverter()
 
-register_album_action(convert_to_simplified)
-register_cluster_action(convert_to_simplified)
-register_file_action(convert_to_simplified)
-register_track_action(convert_to_simplified)
 
-register_album_action(convert_to_traditional)
-register_cluster_action(convert_to_traditional)
-register_file_action(convert_to_traditional)
-register_track_action(convert_to_traditional)
+def convert_to_simplified_chinese(parser, text):
+    if not text:
+        return ""
+    return simplified_converter.convert(text)
+
+
+def convert_to_traditional_chinese(parser, text):
+    if not text:
+        return ""
+    return traditional_converter.convert(text)
+
+
+register_album_action(simplified_converter)
+register_cluster_action(simplified_converter)
+register_file_action(simplified_converter)
+register_track_action(simplified_converter)
+
+register_album_action(traditional_converter)
+register_cluster_action(traditional_converter)
+register_file_action(traditional_converter)
+register_track_action(traditional_converter)
+
+register_script_function(convert_to_simplified_chinese)
+register_script_function(convert_to_traditional_chinese)
