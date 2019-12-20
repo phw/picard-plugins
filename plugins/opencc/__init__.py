1c830a3c59e4e5e7e47d76cf546416ec3f80c335
Philipp Wolfer <phil@parolu.io>
2019-12-20 10:44:31 +0100

    opencc: Updated description

--- plugins/opencc/__init__.py
+++ plugins/opencc/__init__.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 Philipp Wolfer
+# Copyright 2019 Philipp Wolfer < ph.wolfer@gmail.com >
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 PLUGIN_NAME = 'Chinese script conversion'
 PLUGIN_AUTHOR = 'Philipp Wolfer'
-PLUGIN_DESCRIPTION = ('Convert track listings between Traditional Chinese and'
-                      'Simplified Chinese script.')
-PLUGIN_VERSION = "1.1.1"
+PLUGIN_DESCRIPTION = ('Convert track listings between Traditional Chinese and '
+                      'Simplified Chinese script.<br><br>'
+                      'The conversion can be done manually or with scripting. '
+                      'For manual use right click on album, tracks, clusters '
+                      'or files and choose the "Convert to Simplified Chinese" '
+                      'or "Convert to Simplified Chinese" action.<br><br>'
+                      'For scripting you can use the following tagger functions:'
+                      '<ul><li><code>$convert_to_simplified_chinese(text)</code></li>'
+                      '<li><code>$convert_to_traditional_chinese(text)</code></li></ul>'
+                      )
+PLUGIN_VERSION = "1.1.2"
 PLUGIN_API_VERSIONS = ["2.0", "2.1", "2.2", "2.3"]
 PLUGIN_LICENSE = "MIT"
-PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+PLUGIN_LICENSE_URL = "https://opensource.org/licenses/MIT"
 
 
 from picard.album import Album
