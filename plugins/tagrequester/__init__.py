4a005ad6d2b069bf3a69ddf9a1e99cfbfe28e636
Philipp Wolfer <ph.wolfer@gmail.com>
2021-02-08 16:09:30 +0100

    tagrequester: multithreaded access to the named pipe
    Allows for faster processing of multiple files. Refactored code to make it more modular.

--- plugins/tagrequester/__init__.py
+++ plugins/tagrequester/__init__.py
@@ -23,16 +23,14 @@ PLUGIN_DESCRIPTION = (
     'Use the TagRequester audio fingerprinting to fill basic tags.<br><br>'
     'Requires installing and running <a href="http://www.peter-ebe.com/TagRequester/info/tagrequester_download_en.php">TagRequester</a>.'
 )
-PLUGIN_VERSION = '0.1'
+PLUGIN_VERSION = '0.2'
 PLUGIN_API_VERSIONS = ['2.5', '2.6']
 PLUGIN_LICENSE = "GPL-2.0-or-later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 from functools import partial
 from io import FileIO
-from typing import Tuple
-
-from PyQt5.QtCore import QThreadPool
+from time import sleep
 
 from picard import log
 from picard.file import File
@@ -53,10 +51,9 @@ from picard.ui.itemviews import (
     register_cluster_action,
     register_clusterlist_action,
     register_file_action,
-    register_track_action,
+    # register_track_action,
 )
 
-TAG_REQUESTER_PIPE = r'\\.\pipe\TagRequester'
 TAG_MAPPING = {
     'TITLE': 'title',
     'ARTIST': 'artist',
@@ -69,72 +66,87 @@ TAG_MAPPING = {
 }
 
 
+class TagRequesterError(Exception):
+    pass
+
+
+class TagRequesterQuery:
+
+    TAG_REQUESTER_PIPE = r'\\.\pipe\TagRequester'
+
+    def __init__(self, file: File):
+        self.file = file
+
+    def __call__(self) -> XmlNode:
+        response = self._query()
+        self._check_response(response)
+        return response
+
+    def _query(self) -> XmlNode:
+        with self._open_pipe() as pipe:
+            request = self._build_request()
+            log.debug('[TagRequester] Request %s', request)
+            pipe.write(request)
+            response = b''
+            for line in pipe:
+                response += line
+                if line.rstrip().endswith(b'</clientapi>'):
+                    break
+            log.debug('[TagRequester] Query response %s', response)
+            return parse_xml(response)
+
+    def _open_pipe(self) -> FileIO:
+        # Parallel opening of the pipe can fail, wait a bit and try again.
+        # Once the pipe has been opened TagRequester allows for parallel
+        # processing of requests.
+        for retry in range(10):
+            try:
+                return open(self.TAG_REQUESTER_PIPE, 'rb+', buffering=0)
+            except OSError:
+                log.warning('[TagRequester] Accessing named pipe failed, retry %i', retry + 1)
+                sleep(0.1)
+        else:
+            raise TagRequesterError('Failed accessing named pipe %s' % self.TAG_REQUESTER_PIPE)
+
+    def _build_request(self) -> bytes:
+        filename = htmlescape(self.file.filename).encode('utf-8')
+        return (b'<?xml version="1.0" encoding="UTF-8"?>\n'
+                b'<clientapi><FILENAME>%s</FILENAME></clientapi>') % filename
+
+    @staticmethod
+    def _check_response(node: XmlNode):
+        if 'clientapi' not in node.children:
+            raise TagRequesterError('<clientapi> not found in response')
+        clientapi = node.children['clientapi'][0]
+        if 'ERROR' in clientapi.children:
+            raise TagRequesterError(clientapi.children['ERROR'][0].text)
+
+
 class TagRequesterAction(BaseAction):
     NAME = 'TagRequester fingerprinting...'
 
     def __init__(self):
         super().__init__()
-        self.thread_pool = QThreadPool()
-        self.thread_pool.setMaxThreadCount(1)  # Avoid parallel access to the named PIPE
 
     def callback(self, objs):
-        thread.run_task(
-            partial(self.tag_request, objs),
-            self.finish_callback,
-            thread_pool=self.thread_pool)
+        for file in iter_files_from_objects(objs):
+            request = TagRequesterQuery(file)
+            thread.run_task(request, partial(self.handle_response, file))
 
-    def finish_callback(self, result=None, error=None):
+    def handle_response(self, file: File, result=None, error=None):
         if error:
             log.error('[TagRequester] Error: %s', error)
             self.tagger.window.set_statusbar_message(
-                "TagRequester failed: %s", error)
+                'TagRequester failed: %s', error)
             return
-
-    def tag_request(self, objs):
-        with open(TAG_REQUESTER_PIPE, 'rb+', buffering=0) as pipe:
-            for file in iter_files_from_objects(objs):
-                response = self.query(pipe, file.filename)
-                self.handle_response(file, response)
-
-    def query(self, pipe: FileIO, filename: str) -> XmlNode:
-        request = self.build_request(filename)
-        log.debug('[TagRequester] Request %s', request)
-        pipe.write(request)
-        response = b''
-        for line in pipe:
-            response += line
-            if line.rstrip().endswith(b'</clientapi>'):
-                break
-        log.info('[TagRequester] Query response %s', response)
-        return parse_xml(response)
-
-    @staticmethod
-    def build_request(filename: str) -> bytes:
-        return (b'<?xml version="1.0" encoding="UTF-8"?>\n'
-                b'<clientapi><FILENAME>%s</FILENAME></clientapi>') % htmlescape(filename).encode('utf-8')
-
-    def handle_response(self, file: File, response: XmlNode):
-        success, error = self.check_response(response)
-        if not success:
-            log.error('[TagRequester] Error: %s', error)
-            return
-        metadata = self.xml_to_metadata(response)
+        metadata = self.xml_to_metadata(result)
         thread.to_main(partial(self.update_metadata, file, metadata))
 
     def update_metadata(self, file: File, metadata: Metadata):
         self.tagger.window.set_statusbar_message(
-            "TagRequester found metadata for %s", file.filename)
+            'TagRequester found metadata for %s', file.filename)
         file.metadata.update(metadata)
         file.update()
-
-    @staticmethod
-    def check_response(node: XmlNode) -> Tuple[bool, str]:
-        if 'clientapi' not in node.children:
-            return False, '<clientapi> not found in response'
-        clientapi = node.children['clientapi'][0]
-        if 'ERROR' in clientapi.children:
-            return False, clientapi.children['ERROR'][0].text
-        return True, None
 
     @staticmethod
     def xml_to_metadata(node: XmlNode) -> Metadata:
@@ -151,4 +163,4 @@ register_album_action(TagRequesterAction())
 register_cluster_action(TagRequesterAction())
 register_clusterlist_action(TagRequesterAction())
 register_file_action(TagRequesterAction())
-register_track_action(TagRequesterAction())
+# register_track_action(TagRequesterAction())
